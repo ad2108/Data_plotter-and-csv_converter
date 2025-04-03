82608656f0d4a99905a54d6864bd747b0244c8eb
ad2108 <tueckeboldt@gmail.com>
2025-04-03 19:40:20 +0200

    converter update

--- csv_converter/abaqus_to_csv.py
+++ csv_converter/abaqus_to_csv.py
@@ -1,23 +1,28 @@
 # to csv rewriter
 
 print("Csv rewriter:")
-print("Name of the file:")
-name = input()
+print("To exit type exit")
 
-file = open(name, "r")
-newfile = open(name.replace(".txt", ".csv"), "w")
+while True:
+  print("Name of the file:")
+  name = input()
+  if(name == "exit"):
+    break
+
+  file = open(name, "r")
+  newfile = open(name.replace(".txt", ".csv").replace(".rpt", ".csv"), "w")
 
 
-unsorted = []
-for line in file:
-    unsorted.append(line.replace(";", " ").replace(",", " ").split(" "))
+  unsorted = []
+  for line in file:
+      unsorted.append(line.replace(";", " ").replace(",", " ").split(" "))
 
-for element in unsorted:
-    for value in element:
-        if value != "":
-            if value == '\n':
-                newfile.write(value)
-            else:
-                newfile.write(value + ";")
+  for element in unsorted:
+      for value in element:
+          if value != "":
+              if value == '\n':
+                  newfile.write(value)
+              else:
+                  newfile.write(value + ";")
 
-print("Converted")
+  print("Converted")
